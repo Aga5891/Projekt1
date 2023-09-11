3fe53613e676541657780545e845bb4717c46a18
Aga5891 <144334047+Aga5891@users.noreply.github.com>
2023-09-11 10:49:27 +0200

    zadanie dzien 4

--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var age = 33;
+string sex = "kobieta";
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (age < 18 && sex != "kobieta")
+{
+    Console.WriteLine(" Niepełnoletni mężczyzna");
+}
