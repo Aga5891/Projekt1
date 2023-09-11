f25936406ff3180b662d7ee1d6444524e6a09732
Aga5891 <144334047+Aga5891@users.noreply.github.com>
2023-09-11 11:38:42 +0200

    zadanie dzien 4

--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -10,9 +10,13 @@ if (sex == "kobieta" && age < 30)
 }
 else if (name == "Ewa" && age == 33)
 {
-    Console.WriteLine("Ewa, lat 30");
+    Console.WriteLine("Ewa, lat 33");
 }
 else if (age < 18 && sex != "kobieta")
 {
     Console.WriteLine(" Niepełnoletni mężczyzna");
+}
+else if (age > 30 && name != "Ewa") 
+{
+    Console.WriteLine("kobieta po 30");
 }
